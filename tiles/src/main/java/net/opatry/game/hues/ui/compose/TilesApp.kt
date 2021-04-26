89b6b03a8087f3efef10292196d116000beeb1b0
Olivier Patry <olivier.patry@gmail.com>
2021-04-26 13:57:55 +0200

    wip

--- tiles/src/main/java/net/opatry/game/hues/ui/compose/TilesApp.kt
+++ tiles/src/main/java/net/opatry/game/hues/ui/compose/TilesApp.kt
@@ -22,100 +22,53 @@
 
 package net.opatry.game.hues.ui.compose
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.Objects
 import kotlin.math.pow
 
 
-enum class TileUpdate {
-    NEW,
-    NONE,
-    MOVED_UP,
-    MOVED_DOWN,
-    MOVED_LEFT,
-    MOVED_RIGHT,
-    COMBINED;
-
-    override fun toString(): String = when (this) {
-        NEW -> "+"
-        NONE -> " "
-        COMBINED -> "×"
-        MOVED_UP -> "↑"
-        MOVED_DOWN -> "↓"
-        MOVED_LEFT -> "←"
-        MOVED_RIGHT -> "→"
-    }
-}
-
-data class Tile(val rank: Int) : Comparable<Tile> {
+class Tile(val rank: Int) {
     val id: Long = ID++
 
     // Empty tile value is 0 despite 2^0 == 1
     val value: Int = if (rank != 0) 2.0.pow(rank).toInt() else 0
-    var update: TileUpdate = if (rank != 0) TileUpdate.NEW else TileUpdate.NONE
-
-    // TODO should we consider update in compare & equals/hashCode?
-
-    override fun compareTo(other: Tile): Int {
-        // empty tiles are considered equals
-        return if (rank == 0 && other.rank == 0) {
-            0
-        } else {
-            (id - other.id).toInt()
-        }
-    }
-
-    override fun hashCode(): Int {
-        return if (rank == 0) {
-            Objects.hashCode(EMPTY_TILE.id)
-        } else {
-            Objects.hashCode(id)
-        }
-    }
+    var line: Int = 0
+    var column: Int = 0
 
     override fun equals(other: Any?): Boolean {
-        return if (other is Tile) {
-            // empty tiles are considered equals
-            when (rank) {
-                0 -> other.rank == rank
-                else -> id == other.id
-            }
-        } else {
-            false
-        }
+        return super.equals(other)
     }
 
     override fun toString(): String {
-        return "$update${rank.toString().padStart(2, ' ')} (id=${id.toString().padStart(2, ' ')})"
+        return "${rank.toString().padStart(2, ' ')} (id=${id.toString().padStart(2, ' ')})"
     }
 
     companion object {
@@ -126,47 +79,76 @@ data class Tile(val rank: Int) : Comparable<Tile> {
     }
 }
 
+
+val lineCount = 4
+val columnCount = 4
+val tileWidth = 60.dp
+val tileHeight = 80.dp
+
 @Composable
 fun TilesApp() {
     MaterialTheme {
-        var tiles by remember { mutableStateOf(listOf(
-            Tile(0),
-            Tile(1),
-            Tile(2),
-            Tile(3),
-        ))
+        var tile by remember {
+            mutableStateOf(Tile(2).apply {
+                line = 0
+                column = 0
+            })
         }
-        Column {
-            Tiles(tiles)
-            Button(onClick = {
-                tiles = listOf(
-                    Tile(0),
-                    Tile(2).apply { update = TileUpdate.MOVED_LEFT },
-                    Tile(3).apply { update = TileUpdate.NEW },
-                    Tile(4).apply { update = TileUpdate.COMBINED })
-            }) {
-                Text("Change")
-            }
-        }
-    }
-}
 
-@Composable
-fun Tiles(tiles: List<Tile>) {
-    Box(contentAlignment = Alignment.Center) {
-        LazyRow(Modifier.fillMaxWidth()) {
-            items(tiles) { tile ->
+        var line by remember { mutableStateOf(tile.line) }
+        var column by remember { mutableStateOf(tile.column) }
+
+        val diffLine = tile.line - line
+        val diffColumn = tile.column - column
+//        if (diffLine != 0 || diffColumn != 0) {
+        println("diffLine = $diffLine")
+        println("diffColumn = $diffColumn")
+
+        println("LINEBEFOR=$line vs ${tile.line}")
+        line = tile.line
+        println("LINEAFTER=$line vs ${tile.line}")
+        column = tile.column
+//        }
+
+        val offsetX = tileWidth * column
+        val offsetY = tileHeight * line
+
+//        println("COLU=$column OFFSETX=$offsetX")
+//        println("LINE=$line OFFSETY=$offsetY")
+
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(Modifier.size(tileWidth * lineCount, tileHeight * columnCount)) {
                 TileView(
                     tile,
                     Modifier
-                        .fillMaxWidth()
-                        .size(60.dp, 80.dp)
+                        .size(tileWidth, tileHeight)
+                        .offset(offsetX, offsetY)
                 )
+            }
+
+            Button(onClick = { tile = tile.apply { line -= 1 } }, enabled = line > 0) {
+                Icon(Icons.Default.KeyboardArrowUp, "Up")
+            }
+            Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceAround) {
+                Button(onClick = { tile = tile.apply { column -= 1 } }, enabled = column > 0) {
+                    Icon(Icons.Default.KeyboardArrowLeft, "Left")
+                }
+                Button(
+                    onClick = { tile = tile.apply { line += 1 } },
+                    enabled = line < lineCount - 1
+                ) {
+                    Icon(Icons.Default.KeyboardArrowDown, "Down")
+                }
+                Button(
+                    onClick = { tile = tile.apply { column += 1 } },
+                    enabled = column < columnCount - 1
+                ) {
+                    Icon(Icons.Default.KeyboardArrowRight, "Right")
+                }
             }
         }
     }
 }
-
 
 val TileColors = listOf(
     Color(0xFFFFFFFF),
@@ -181,87 +163,11 @@ fun TileView(
     tile: Tile,
     modifier: Modifier = Modifier
 ) {
-    val duration = 600
     var size by remember { mutableStateOf(IntSize.Zero) }
-    val scale = remember(
-        tile.id,
-        tile.update
-    ) { Animatable(if (tile.update == TileUpdate.NEW) 0f else 1f) }
-    val angle = remember(tile.id, tile.update) { Animatable(0f) }
-    val offsetX = remember(tile.id, tile.update) {
-        Animatable(
-            when (tile.update) {
-                TileUpdate.MOVED_LEFT -> size.width.toFloat()
-                TileUpdate.MOVED_RIGHT -> -size.width.toFloat()
-                else -> 0f
-            }
-        )
-    }
-    val offsetY =
-        remember(tile.id, tile.update) {
-            Animatable(
-                when (tile.update) {
-                    TileUpdate.MOVED_UP -> size.height.toFloat()
-                    TileUpdate.MOVED_DOWN -> -size.height.toFloat()
-                    else -> 0f
-                }
-            )
-        }
-    LaunchedEffect(tile) {
-        when (tile.update) {
-            TileUpdate.NONE -> {
-                scale.snapTo(1f)
-                angle.snapTo(0f)
-                offsetX.snapTo(size.width.toFloat())
-                offsetY.snapTo(size.height.toFloat())
-            }
-            TileUpdate.NEW -> {
-                scale.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(duration)
-                )
-                angle.snapTo(0f)
-                offsetX.snapTo(0f)
-                offsetY.snapTo(0f)
-            }
-            TileUpdate.COMBINED -> {
-                scale.snapTo(1f)
-                angle.animateTo(
-                    targetValue = 180f, // FIXME 180f mirrors the card, what we want is tile flip effect without mirroring
-                    animationSpec = tween(duration)
-                )
-                offsetX.snapTo(0f)
-                offsetY.snapTo(0f)
-            }
-            TileUpdate.MOVED_UP,
-            TileUpdate.MOVED_DOWN -> {
-                scale.snapTo(1f)
-                angle.snapTo(0f)
-                offsetX.snapTo(0f)
-                offsetY.animateTo(
-                    targetValue = 0f,
-                    animationSpec = tween(duration)
-                )
-            }
-            TileUpdate.MOVED_LEFT,
-            TileUpdate.MOVED_RIGHT -> {
-                scale.snapTo(1f)
-                angle.snapTo(0f)
-                offsetX.animateTo(
-                    targetValue = 0f,
-                    animationSpec = tween(duration)
-                )
-                offsetY.snapTo(0f)
-            }
-        }
-    }
 
     Box(
         modifier
-            .onSizeChanged { if (size == IntSize.Zero) size = it }
-            .scale(scale.value)
-            .graphicsLayer(rotationY = angle.value)
-            .offset(x = offsetX.value.dp, y = offsetY.value.dp),
+            .onSizeChanged { if (size == IntSize.Zero) size = it },
         contentAlignment = Alignment.BottomCenter
     ) {
         // only draw tile slot for empty tiles
@@ -282,7 +188,7 @@ fun TileView(
                 // debug data
                 Box(Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
                     Text(
-                        "${tile.update} id=${tile.id}",
+                        "id=${tile.id}",
                         Modifier.padding(4.dp),
                         color = Color.White,
                         fontSize = 9.sp
